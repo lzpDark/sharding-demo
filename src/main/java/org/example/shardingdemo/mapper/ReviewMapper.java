fd7e853ed734e16328baef90fa79a83ac7e45cbc
lzp <31664299+lzpDark@users.noreply.github.com>
2024-08-10 01:02:28 +0800

    1

---
+++ src/main/java/org/example/shardingdemo/mapper/ReviewMapper.java
@@ -0,0 +1,10 @@
+package org.example.shardingdemo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.example.shardingdemo.entity.Review;
+
+/**
+ * @author lzp
+ */
+public interface ReviewMapper extends BaseMapper<Review> {
+}
